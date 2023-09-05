9bfaed2852ad4ddc324f483e3e23d8dc76defe5c
giegiewealth <angelamarfo17@gmail.com>
2023-09-05 16:19:57 -0700

    ascii 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for ascii_value in range(97, 123):
     if chr(ascii_value) not in ['q', 'e']:
-        print(chr(ascii_value), end='')
+        print("{}".format(chr(ascii_value)), end='')
