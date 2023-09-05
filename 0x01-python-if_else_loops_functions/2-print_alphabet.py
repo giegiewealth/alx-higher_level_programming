72c6b45db47fda4d1a0d0744bcffa027c1f9a621
giegiewealth <angelamarfo17@gmail.com>
2023-09-05 14:13:35 -0700

    ascii

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for ascii_value in range(97, 123):
-    print(chr(ascii_value), end='')
+
+alphabet = ''.join(chr(ascii_value) for ascii_value in range(97, 123))
+print(alphabet)
