423e7d723074f5ceda3959fa2efcb032292da2ff
giegiewealth <angelamarfo17@gmail.com>
2023-09-05 17:41:09 -0700

    add pls

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
+toggle = False
+
 for i in range(ord('z'), ord('A') - 1, -1):
     char = chr(i)
 
-    # Toggle between lowercase and uppercase
-    if char.islower():
-        char = char.upper()
-    else:
-        char = char.lower()
+    if toggle:
+        char = char.swapcase()
 
     print(char, end='')
+
+    toggle = not toggle
 
 # Print a newline character to end the line
 print()
