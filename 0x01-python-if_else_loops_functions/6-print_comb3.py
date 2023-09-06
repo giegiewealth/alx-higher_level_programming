d8f548d6cca52e26247fbbef2a26345d07b5514d
giegiewealth <angelamarfo17@gmail.com>
2023-09-05 17:00:13 -0700

    print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i+1, 10):
-        print("{:02d}, {:02d}, ".format(i, j), end='')
 
-print("{:02d}".format(89))
+for tens_digit in range(10):
+    for ones_digit in range(tens_digit + 1, 10):
+        print("{:d}{:d}, ".format(tens_digit, ones_digit), end="")
+
+print("{:d}{:d}".format(9, 9))
