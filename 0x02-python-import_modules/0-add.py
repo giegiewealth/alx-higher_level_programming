ba3a1601d0699c46a55e4bccf5329d9ac62a678d
giegiewealth <angelamarfo17@gmail.com>
2023-09-07 21:30:49 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
-# Assign values to variables a and b
-a = 1
-b = 2
-
-from add_0 import add
-
-# Calculate the result using the imported add function
-result = add(a, b)
-
-# Check if this script is the main program
 if __name__ == "__main__":
-    # Print the result using string formatting
-    print("{} + {} = {}".format(a, b, result))
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
