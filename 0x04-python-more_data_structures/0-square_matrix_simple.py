7bd06f1d749e5d19affae0103b96d5111ba4a4ac
giegiewealth <angelamarfo17@gmail.com>
2023-09-14 02:28:02 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Get the number of rows and columns in the input matrix
-    num_rows = len(matrix)
-    num_cols = len(matrix[0]) if num_rows > 0 else 0
+    new_matrix = matrix.copy()
 
-    # Create a new matrix to store the squared values
-    result_matrix = [[0] * num_cols for _ in range(num_rows)]
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-    # Iterate through the rows and columns of the input matrix
-    for i in range(num_rows):
-        for j in range(num_cols):
-            # Square the value at the current position and store it in the result matrix
-            result_matrix[i][j] = matrix[i][j] ** 2
-
-    return result_matrix
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-result = square_matrix_simple(matrix)
-for row in result:
-    print(row)
+    return (new_matrix)
