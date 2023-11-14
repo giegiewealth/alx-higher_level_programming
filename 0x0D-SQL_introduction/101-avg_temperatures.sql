8b635253a1cea4f647dc788a1380434bbfb16c95
giegiewealth <angelamarfo17@gmail.com>
2023-11-14 15:05:55 -0800

    update

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,10 +1,7 @@
--- displays the average temp of 3 cities
--- during July and August
+-- displays teh average temp by city
 -- ordered by temperature desc
 
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+ORDER BY avg_temp DESC;
